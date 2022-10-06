89f688874d1626c52612a903909d20a3216ca21f
Viper20201998 <lesterasmethsandoval@gmail.com>
2022-10-06 12:56:38 -0600

    term add Ctrl+

--- lua/toggleterm-config/init.lua
+++ lua/toggleterm-config/init.lua
@@ -1,10 +1,10 @@
 require("toggleterm").setup({
-	open_mapping = [[<c-\>]],
-	size = 55,
-	shade_terminals = true,
-	direction = "vertical",
-	-- float_opts = {
-	-- 	border = "double",
-	-- },
-	close_on_exit = true,
+    open_mapping = [[<c-\>]],
+    size = 55,
+    shade_terminals = true,
+    direction = "float",
+    -- float_opts = {
+    -- 	border = "double",
+    -- },
+    close_on_exit = true,
 })
