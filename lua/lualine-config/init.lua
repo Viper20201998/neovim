e711a1946de776bc0be128017609efed90127008
Viper20201998 <lesterasmethsandoval@gmail.com>
2022-10-06 23:53:27 -0600

    modi

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,11 +1,11 @@
 require('lualine').setup {
     options = {
-        component_separators = { left = '', right = '' },
+        component_separators = { left = ' ', right = ' ' },
         section_separators = { left = ' ', right = ' ' },
     },
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = { 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
